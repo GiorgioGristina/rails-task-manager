706c0d907bcbc8d5b9f658eceead1ab7ae3976f1
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-08 13:22:31 +0100

    added routes to display only one task and created action show in the controller and add show.html.erb view file for tasks and display data on the page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # for all the task
   get '/tasks', to: 'tasks#index'
+  # for one the task
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+
 end
