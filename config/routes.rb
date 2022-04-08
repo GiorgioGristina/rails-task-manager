2c2c613bdadcb0d1a24eec4c9db8aab548f6af7b
Giorgio Gristina <giorgio.gristina91@gmail.com>
2022-04-08 15:11:00 +0100

    added routes to create new task and created action new and create in the controller and add new.html.erb view file for tasks and display form. add bootstrap and gem for simpleform

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # for all the task
   get '/tasks', to: 'tasks#index'
+  # for create a new task
+  get '/tasks/new', to: 'tasks#new', as: 'new'
+  post '/tasks', to: 'tasks#create'
   # for one the task
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_111854) do
+ActiveRecord::Schema.define(version: 2022_04_08_140231) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
